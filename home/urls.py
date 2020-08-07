77c65c3df444bc85508bdda06836f7dd7fab3bcb
Zulfikar Ditya <zulfikarditya.work@gmail.com>
2020-08-07 20:41:54 +0700

    add login/ register and setup another pages

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,6 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('you_are_logged_in/', views.you_are_loged_in, name='logged_in'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 
+from .forms import RegisterForm
+
+
+def register(request):
+    if request.user.is_authenticated:    
+        
+        return HttpResponseRedirect(reverse('home:logged_in'))
+    else:
+        if request.method == 'POST':
+            form = RegisterForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect('../login/')
+        else:
+            form = RegisterForm()
+        return render(request, 'registration/create_account.html', {'form': form})
+
+
+def you_are_loged_in(request):
+    return render(request, 'home/logged_in.html')
 
 def index(request):
     return render(request, 'home/index.html')
